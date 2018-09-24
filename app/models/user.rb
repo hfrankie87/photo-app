b160e320894f1548de7989764ddb1687aecba016
Frank Hernandez <hfrank0714@gmail.com>
2018-09-24 00:24:07 +0000

    Add production image upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
          
   has_one :payment
   accepts_nested_attributes_for :payment 
+  has_many :images
 end
